da1d689bf3aba3700fddbe355dd80d03c12c72ce
Adonat74 <donatgoninet.antoine@gmail.com>
2024-09-04 11:50:00 +0200

    made the first css and html prep and made the footer and header

---
+++ src/components/headerFooter/footer.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    <div>
+      <div class="socialNetworkLinks">
+        <img src="../../assets/icons/instagram.svg" alt="instagram" width="30" height="30" />
+        <p>instagram/be-queen.fr</p>
+      </div>
+      <div class="socialNetworkLinks marginTop">
+        <img src="../../assets/icons/facebook.svg" alt="facebook" width="30" height="30" />
+        <p>facebook/be-queen.fr</p>
+      </div>
+    </div>
+    <div>
+      <div class="socialNetworkLinks">
+        <img src="../../assets/icons/linkedin.svg" alt="linkedin" width="30" height="30" />
+        <p>linkedin/be-queen.fr</p>
+      </div>
+      <div class="socialNetworkLinks marginTop">
+        <img src="../../assets/icons/adress.svg" alt="adress" width="30" height="30" />
+        <p>adress/be-queen.fr</p>
+      </div>
+    </div>
+    <p>&copy; copyright by be queen team</p>
+    <div>
+      <p>linkedin@gmail.fr</p>
+      <p class="marginTop">06 01 23 45 67</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+    footer {
+    background-color: var(--black);
+    height: 100px;   
+    color: var(--white);
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .socialNetworkLinks {
+    display: flex;
+    align-items: center;
+  }
+
+  .socialNetworkLinks > img {
+    margin-right: 10px;
+  }
+
+  .marginTop {
+    margin-top: 10px;
+  }
+</style>
---
+++ src/components/headerFooter/header.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <img alt="be Queen logo" class="logo" src="@/assets/images/logo.png" width="125" height="125" />
+    <nav>
+      <RouterLink to="/">ACCUEIL</RouterLink>
+      <a href="">CATALOGUE</a>
+      <a href="">PERSONALISATION</a>
+      <a href="">CONTACT</a>
+      <a href=""><img src="../../assets/icons/cart2.svg" alt="cart icon" width="30" height="30" /></a>
+      <a href=""
+        ><img src="../../assets/icons/account.svg" alt="account icon" width="30" height="30"
+      /></a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+    header {
+    background-color: var(--green);
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .logo {
+    position: relative;
+    left: 20px;
+    top: 30px;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 1.15rem;
+    width: 650px;
+    margin-right: 20px;
+  }
+</style>
